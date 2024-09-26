79a42e27249bd1cfcf10ff8a529668337af1fdb1
C-Shart <aaron.blalack@gmail.com>
2024-09-27 07:01:30 +0900

    Got it working albeit w deprecated event method

--- Weather/openweather.js
+++ Weather/openweather.js
@@ -19,16 +19,17 @@ const temperatureElement = document.getElementById('temperature');
 const descriptionElement = document.getElementById('description');
 
 searchButton.addEventListener('click', () => {
+    event.preventDefault();
     const location = locationInput.value;
     if (location) {
         fetchWeatherByGeo(location);
     }
 });
 
-async function fetchGeo(location) {
-    var url;
-
-    zipCode = zipRe.test(location)
+const fetchGeo = async location => {
+    let url;
+    let geoData;
+    const zipCode = zipRe.test(location);
 
     if (zipCode) {
         url = `${zipUrl}?zip=${location}&appid=${apiKey}`;
@@ -41,140 +42,44 @@ async function fetchGeo(location) {
     console.log(`ziptest: ${zipCode}`)
     console.log(`url: ${url}`)
 
-    geoResponse = await fetch(url);
-    geoData = await geoResponse.json();
+    try {
+        const geoResponse = await fetch(url, {method: "GET"});
+        if (geoResponse.ok) {
+            geoData = await geoResponse.json();
 
-    console.log(`geoData: ${geoData}`)
+            console.log(`geoResponse: ${geoResponse}`);
+            console.log(`geoData: ${geoData[0].name}`);
+            console.log(`geoData: ${geoData[0].lat}`);
+            console.log(`geoData: ${geoData[0].lon}`);
+        }
+    } catch(error) {
+        console.log(error);
+    }
+    return geoData;
+};
 
-    return geoData
+const fetchWeatherByGeo = async inputLocation => {
+    let weatherData;
 
-    /* 
-    return fetch(url)
-        .then((geoResponse) => geoResponse.json())
-        .then((data) => console.log(data))
-        .catch((error) => {
-            console.error('Error fetching geo data:', error);
-        });
-    */
-}
+    try {
+        const fetchedGeoData = await fetchGeo(inputLocation);
+        
+        if (fetchedGeoData) {
+            const weatherUrl = `${currentWeatherBaseUrl}?lat=${fetchedGeoData[0].lat}&lon=${fetchedGeoData[0].lon}&exclude=minutely&units=metric&appid=${apiKey}`;
 
-async function fetchWeatherByGeo(inputLocation) {
-    const fetchedGeoData = await fetchGeo(inputLocation);
-    const weatherUrl = `${currentWeatherBaseUrl}?lat=${fetchedGeoData.lat}&lon=${fetchedGeoData.long}&exclude=minutely&units=metric&appid=${apiKey}`;
+            console.log(`weatherUrl: ${weatherUrl}`)
 
-    console.log(`weatherUrl: ${weatherUrl}`)
-
-    weatherResponse = await fetch(weatherUrl);
-    weatherData = await weatherResponse.json();
-
-    console.log(`Weather Data: ${weatherData.name}, ${Math.round(weatherData.main.temp)}°C, ${weatherData.weather[0].description}`)
-
-    /* locationElement.textContent = weatherData.name;
-    temperatureElement.textContent = `${Math.round(data.main.temp)}°C`;
-    descriptionElement.textContent = data.weather[0].description;
-
-    fetch(weatherUrl)
-        .then((response) => response.json())
-        .then((weatherData) => {
+            // Try to fetch the weather data
+            const weatherResponse = await fetch(weatherUrl);
+            if (weatherResponse.ok) {
+                weatherData = await weatherResponse.json();
+            }
+            console.log(`Weather Data: ${weatherData.name}, ${Math.round(weatherData.main.temp)}°C, ${weatherData.weather[0].description}`)
             locationElement.textContent = weatherData.name;
-            temperatureElement.textContent = `${Math.round(data.main.temp)}°C`;
-            descriptionElement.textContent = data.weather[0].description;
-        })
-        .catch((error) => {
-            console.error('Error fetching weather data:', error);
-            console.error('HTTP Response:', response?.status);
-        }); */
-}
-
-// Autocomplete function stuff, currently non functional
-
-/*
-function autocomplete(input, cities) {
-    var currentFocus;
-
-    input.addEventListener("input", function(e) {
-        var autocompleteDiv, matchDiv, i, val = this.value;
-        closeAllLists();
-
-        if (!val) { return false;}
-        currentFocus = -1;
-
-        // Creates a div that contains the values
-        autocompleteDiv = document.createElement("DIV");
-        autocompleteDiv.setAttribute("id", this.id + "autocomplete-list");
-        autocompleteDiv.setAttribute("class", "autocomplete-items");
-
-        // Appends div as child of the autocomplete container
-        this.parentNode.appendChild(autocompleteDiv);
-
-        for (i = 0; i < cities.length; i++) {
-            if (cities[i].substr(0, val.length).toUpperCase() == val.toUpperCase()) {
-
-                matchDiv = document.createElement("DIV");
-                matchDiv.innerHTML = "<strong>" + cities[i].substr(0, val.length) + "</strong>";
-                matchDiv.innerHTML += cities[i].substr(val.length);
-
-                // Inserts input field that holds the current array item's value:
-                matchDiv.innerHTML += "<input type='hidden' value='" + cities[i] + "'>";
-
-                // Inserts value when clicked & closes the list
-                matchDiv.addEventListener("click", function(e) {
-                    input.value = this.getElementsByTagName("input")[0].value;
-                    closeAllLists();
-                });
-                autocompleteDiv.appendChild(matchDiv);
-            }
+            temperatureElement.textContent = `${Math.round(weatherData.main.temp)}°C`;
+            descriptionElement.textContent = weatherData.weather[0].description;
         }
-    });
-
-    // keypress function
-    input.addEventListener("keydown", function(e) {
-        var x = document.getElementById(this.id + "autocomplete-list");
-        if (x) x = x.getElementsByTagName("div");
-        // press DOWN
-        if (e.keyCode == 40) {
-            currentFocus++;
-            addActive(x);
-        // press UP
-        } else if (e.keyCode == 38) { //up
-            currentFocus--;
-            addActive(x);
-        // press ENTER (simulate click)
-        } else if (e.keyCode == 13) {
-            e.preventDefault();
-            if (currentFocus > -1) {
-                if (x) x[currentFocus].click();
-            }
-        }
-    });
-
-    // Classifies item as active
-    function addActive(x) {
-        if (!x) return false;
-        removeActive(x);
-        if (currentFocus >= x.length) currentFocus = 0;
-        if (currentFocus < 0) currentFocus = (x.length - 1);
-        x[currentFocus].classList.add("autocomplete-active");
+    } catch(error) {
+        console.log(error)
     }
-    function removeActive(x) {
-        for (var i = 0; i < x.length; i++) {
-            x[i].classList.remove("autocomplete-active");
-        }
-    }
-
-    function closeAllLists(elmnt) {
-        var x = document.getElementsByClassName("autocomplete-items");
-        for (var i = 0; i < x.length; i++) {
-            if (elmnt != x[i] && elmnt != input) {
-            x[i].parentNode.removeChild(x[i]);
-        }
-    }
-}
-
-document.addEventListener("click", function (e) {
-    closeAllLists(e.target);
-});
-}
-
-autocomplete(document.getElementById("locationInput"), cities);
-*/
+};
